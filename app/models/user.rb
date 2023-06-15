e01124afa9fe2f8fb060972820897cf53dbb9db5
dinhthi12 <thi884641@gmail.com>
2023-06-15 22:27:40 +0700

    chapter_7

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,13 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true,
     length: {minimum: Settings.digits.length_10,
              maximum: Settings.digits.length_30},
-    format: {with: Regexp.new(Settings.user.email.regex)}
+    format: {with: Settings.user.email.regex}
   validates :password, presence: true,
     length: {minimum: Settings.user.password.min_length}
+
+  scope :sort_by_name, lambda {|direct, direct_email|
+                         order(name: direct, email: direct_email)
+                       }
 
   has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_13_163106) do
+ActiveRecord::Schema.define(version: 2023_06_16_065042) do
 
-  create_table "users", charset: "latin1", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
