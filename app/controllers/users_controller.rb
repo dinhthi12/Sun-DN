60216e2bbb0ac84922e090e903f341beb7f177d0
dinhthi12 <thi884641@gmail.com>
2023-06-18 22:38:10 +0700

    chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
-  before_action :load_user, only: :show
+  before_action :logged_in_user, except: %i(show new create)
+  before_action :load_user, except: %i(index new create)
+  before_action :correct_user, only: %i(edit update show)
+  before_action :admin_user, only: :destroy
+
   def index
-    @users = User.sort_by_name(:asc, :asc)
+    @pagy, @users = pagy(User.sort_by_name(:asc, :asc), items:
+        Settings.digits.length_6)
   end
 
   def new
@@ -27,7 +32,46 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t "notification.update"
+      redirect_to @user
+    else
+      flash.now[:warning] = t "notification.not_update"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = "notification.destroy"
+    else
+      flash[:danger] = "notification.not_destroy"
+    end
+    redirect_to users_path
+  end
+
   private
+
+  def correct_user
+    return if current_user? @user
+
+    flash[:error] = "notification.not_edit."
+    redirect_to root_url
+  end
+
+  def action_if_not_logged_in
+    store_location
+    flash[:danger] = t "notification.request_login"
+    redirect_to login_url
+  end
+
+  def logged_in_user
+    action_if_not_logged_in unless logged_in?
+  end
+
   def load_user
     @user = User.find_by id: params[:id]
     return if @user
@@ -36,9 +80,15 @@ class UsersController < ApplicationController
     redirect_to root_path
   end
 
-  private
   def user_params
     params.require(:user).permit :name, :email, :password,
                                  :password_confirmation
+  end
+
+  def admin_user
+    return if current_user.admin?
+
+    redirect_to root_path
+    flash[:danger] = t "notification.not_admin"
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,12 @@
 module UsersHelper
-  def gravatar_for user
+  def gravatar_for user, options = {Size: Settings.digits.size_80}
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    gravatar_url = Settings.user.user_url + gravatar_id
+    options[:size]
+    gravatar_url = Settings.user.gravatar_url + gravatar_id
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+
+  def currentuser_is_admin? user
+    current_user.admin? && !current_user?(user)
   end
 end
